444a522cb202679e5859280cd6f8b2e8ca98b7c7
song <brownmole01@naver.com>
2023-09-04 12:48:54 +0900

    0904

---
+++ 4_Excel/script.js
@@ -0,0 +1,6 @@
+document.getElementById("sheetjsexport").addEventListener('click', function () {
+    /* Create worksheet from HTML DOM TABLE */
+    var wb = XLSX.utils.table_to_book(document.getElementById("TableToExport"));
+    /* Export to file (start a download) */
+    XLSX.writeFile(wb, "SheetJSTable.xlsx");
+});
