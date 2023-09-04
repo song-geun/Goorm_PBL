d0e1e390049709fdb6157a432b56d39cfe1e5b51
song <brownmole01@naver.com>
2023-09-04 17:01:09 +0900

    check

--- 4_Excel/Excel.js
+++ 4_Excel/Excel.js
@@ -2,21 +2,23 @@ class Excel {
     Excel
     constructor() {
         this.Excel = document.querySelector("#TableToExport");
-        let sheet;
-        for (let j = 1; j <= 1000; j++) {
-            sheet = document.createElement('TR');
-            sheet.classname = j;
+        
+    }
+    async init()
+    {
+        let sheet=[];
+        for (let j = 1; j <= 100; j++) {
+            sheet[j] = document.createElement('tr');
+            sheet[j].classname = 'a1'+j;
             for (let i = 1; i < 27; i++) {
-                let now = document.createElement('TD');
-                let now1 = document.createElement('span')
-                now1.className = String.fromCharCode(Number(64) + Number(i)) + j;
-                now1.textContent = String.fromCharCode(Number(64) + Number(i)) + j;
-                now.appendChild(now1);
-                now.classname = String.fromCharCode(Number(64) + Number(i)) + j;
+                let now = document.createElement('td');
+                now.id = String.fromCharCode(Number(64) + Number(i)) + j;
+                now.textContent = String.fromCharCode(Number(64) + Number(i)) + j;
+                now.classname = "TD1";
                 
-                sheet.appendChild(now);
+                sheet[j].appendChild(now);
             }
-            this.Excel.appendChild(sheet);
+            this.Excel.appendChild(sheet[j]);
         }
     }
 }
--- 4_Excel/script.js
+++ 4_Excel/script.js
@@ -1,12 +1,19 @@
+excel = new Excel();
+excel.init();
+
 document.getElementById("sheetjsexport").addEventListener('click', function () {
     var wb = XLSX.utils.table_to_book(document.getElementById("TableToExport"));
     XLSX.writeFile(wb, "SheetJSTable.xlsx");
 });
 
-document.querySelector('*').addEventListener('click',
+document.querySelector('#TableToExport').addEventListener('click',
     (e)=> {
         let now = e.target.id;
         console.log(now);
     });
 
-excel = new Excel();
+    document.querySelector('#TableToExport').addEventListener('dbclick',
+    (e)=> {
+        let now = e.target.id;
+        console.log(now);
+    });
