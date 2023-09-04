e92b53224b5e65aca91cf16bd68a22acbb5cad4b
song <brownmole01@naver.com>
2023-09-04 16:37:42 +0900

    check

--- 4_Excel/Excel.js
+++ 4_Excel/Excel.js
@@ -3,22 +3,9 @@ class Excel {
     constructor() {
         this.Excel = document.querySelector("#TableToExport");
         let sheet;
-        sheet = document.createElement('TR');
-        sheet.classname = 0;
-        for (let i = 0; i < 27; i++) {
-            let now = document.createElement('span');
-            now.classname = String.fromCharCode(Number(64) + Number(i));
-            now.textContent = String.fromCharCode(Number(64) + Number(i));
-            sheet.appendChild(now);
-        }
-        this.Excel.appendChild(sheet);
         for (let j = 1; j <= 1000; j++) {
-            sheet = document.createElement('tr');
+            sheet = document.createElement('TR');
             sheet.classname = j;
-            let now = document.createElement('span');
-            now.textContent = j;
-            now.classname = j;
-            this.Excel.append(now)
             for (let i = 1; i < 27; i++) {
                 let now = document.createElement('TD');
                 let now1 = document.createElement('span')
