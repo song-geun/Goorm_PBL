483a10e68a8d2da98c99ec4a12bfb1434929f4af
song <brownmole01@naver.com>
2023-09-17 10:34:30 +0900

    0917

--- 5_react_crud/src/components/Main_list.js
+++ 5_react_crud/src/components/Main_list.js
@@ -20,17 +20,17 @@ export default function Main_list({ listdata, handleedit, setListData }) {
                         <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded shadow">
 
                             <input
-                                className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow "
+                                className="w-full px-2 py-2 mr-4 text-gray-500 border rounded shadow "
                                 id={name + data.id}
                                 placeholder={data.title}
                             />
                             <input
-                                className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow "
+                                className="w-full px-2 py-2 mr-4 text-gray-500 border rounded shadow "
                                 id={value + data.id}
                                 placeholder={data.val}
                             />
-                            <div>
-                                <button onClick={(e) => handleedit(e,data.id)}>edit</button>
+                            <div className="w-full float-right">
+                                <i class="fa-solid fa-pen-to-square" onClick={(e) => handleedit(e,data.id)}></i>
                                 <button onClick={() => handleClick(data.id)}>x</button>
                             </div>
                         </div>
